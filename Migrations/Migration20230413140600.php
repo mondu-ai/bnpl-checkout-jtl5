7013722e7af4e38353b9298eba8dfd0d3aabd275
harism2 <97665980+harism2@users.noreply.github.com>
2023-05-16 17:35:32 +0200

    Update Migration20230413140600.php

--- Migrations/Migration20230413140600.php
+++ Migrations/Migration20230413140600.php
@@ -10,14 +10,22 @@ class Migration20230413140600 extends Migration implements IMigration
     public function up()
     {
         $this->execute("
-          ALTER TABLE `mondu_orders` ADD `authorized_net_term` INT NULL AFTER `updated_at`;
+          CREATE TABLE IF NOT EXISTS `mondu_orders` (
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+              `order_id` int(11) NOT NULL,
+              `state` varchar(256) DEFAULT NULL,
+              `external_reference_id` varchar(256) DEFAULT NULL,
+              `order_uuid` varchar(256) DEFAULT NULL,
+              `authorized_net_term` int(11) DEFAULT NULL,
+              `created_at` datetime DEFAULT NULL,
+              `updated_at` datetime DEFAULT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
         ");
     }
 
     public function down()
     {
-      $this->execute("
-          ALTER TABLE `mondu_orders` DROP COLUMN `authorized_net_term`;
-        ");
+      
     }
 }
