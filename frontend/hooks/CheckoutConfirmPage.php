11b15ad750a91031234e52dc933f3e4b52340610
Tigran Hovhannisyan <tiko.hovhannisyan.and.t@gmail.com>
2023-10-24 12:55:37 +0400

    Changes in CheckoutConfirmPage hook

--- frontend/hooks/CheckoutConfirmPage.php
+++ frontend/hooks/CheckoutConfirmPage.php
@@ -9,17 +9,37 @@ class CheckoutConfirmPage
 {
     public function execute(): void
     {
-        if (isset($_SESSION['Zahlungsart']) && $_SESSION['Zahlungsart']->cAnbieter == 'Mondu') {
-            if (isset($GLOBALS['step']) && $GLOBALS['step'] == 'Bestaetigung') {
-                if (!isset($_SESSION['monduOrderUuid']) || empty($_SESSION['monduOrderUuid']) || $_GET['payment'] != 'accepted') {
-                    $orderService = new OrderService();
-                    $orderData = $orderService->token($_SESSION['Zahlungsart']->cModulId);
+        if (!$this->isMonduPayment() || !$this->isConfirmStep()) return;
 
-                    header('Location: ' . $orderData['hosted_checkout_url'], true, 303);
-                    exit;
-                }
-            }
+        if ($_GET['payment'] !== 'accepted' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+            header('Location: ' . Shop::Container()->getLinkService()->getStaticRoute('bestellvorgang.php') . '?editZahlungsart=1');
         }
+
+        if ($_GET['payment'] == 'accepted' || !$this->isMonduOrderSessionMissing()) return;
+
+        if ($_GET['monduCreateOrder'] !== 'true') {
+            header('Location: ' . Shop::Container()->getLinkService()->getStaticRoute('bestellvorgang.php') . '?monduCreateOrder=true');
+            exit;
+        }
+
+        if ($_GET['monduCreateOrder'] === 'true') {
+            $orderService = new OrderService();
+            $orderData = $orderService->token($_SESSION['Zahlungsart']->cModulId);
+            header('Location: ' . $orderData['hosted_checkout_url'], true, 303);
+            exit;
+        }
+    }
+
+    protected function isMonduPayment() {
+        return isset($_SESSION['Zahlungsart']) && $_SESSION['Zahlungsart']->cAnbieter == 'Mondu';
+    }
+
+    protected function isConfirmStep() {
+        return isset($GLOBALS['step']) && $GLOBALS['step'] == 'Bestaetigung';
+    }
+
+    protected function isMonduOrderSessionMissing() {
+        return !isset($_SESSION['monduOrderUuid']) || empty($_SESSION['monduOrderUuid']);
     }
 }
 
