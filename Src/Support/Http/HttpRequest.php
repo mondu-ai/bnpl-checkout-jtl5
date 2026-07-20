ce4a19d42f84c93e68858222544926fa1697dbdd
Ivan Pugach <ivan.pugach@ext.mondu.ai>
2026-07-20 17:16:05 +0300

    Implement improvements

--- Src/Support/Http/HttpRequest.php
+++ Src/Support/Http/HttpRequest.php
@@ -148,8 +148,6 @@ class HttpRequest
         $response = curl_exec($this->curl);
         $info = curl_getinfo($this->curl);
 
-        curl_close($this->curl);
-
         if (!$response || !($info['http_code'] >= 200 && $info['http_code'] <= 299)) {
             $exception = new \stdClass();
             $exception->method = $method;
